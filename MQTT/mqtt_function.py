9f37754cf335e000e9c0b6a406ac0a5bedf2675f
Jay Clifford <jclifford@influxdata.com>
2023-12-11 17:29:56 +0000

    Edited 'mqtt_function.py'

--- MQTT/mqtt_function.py
+++ MQTT/mqtt_function.py
@@ -34,7 +34,8 @@ class MQTTFunction:
             self.producer_topic.get_or_create_stream(str(topic).replace("/", "-")).events \
                 .add_timestamp(datetime.utcnow()) \
                 .add_value("data", payload.decode("utf-8")) \
-                .add_value("otel", str(otel)) \
                 .add_tag("qos", str(qos)) \
+                .add_tag("trace_id", str(span_context.trace_id)) \
+                .add_tag("span_id", str(span_context.span_id) ) \
                 .publish()
             
