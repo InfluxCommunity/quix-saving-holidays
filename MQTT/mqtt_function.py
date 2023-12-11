f2aa895b951415aa2b0e9c27186b269ea47eb7d6
Jay Clifford <jclifford@influxdata.com>
2023-12-11 13:58:49 +0000

    Edited 'mqtt_function.py'

--- MQTT/mqtt_function.py
+++ MQTT/mqtt_function.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class MQTTFunction:
 
-    def __init__(self, topic, mqtt_client: paho.Client, producer_topic: qx.TopicProducer):
+    def __init__(self, topic, mqtt_client: paho.Client, producer_topic: qx.TopicProducer, tracer):
         self.mqtt_client = mqtt_client
         self.topic = topic
         self.producer_topic = producer_topic
